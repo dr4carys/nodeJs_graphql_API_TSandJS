b22ce981fa2d7d2e1cad6dd7669212aae9aa5c24
dr4carys <48232195+dr4carys@users.noreply.github.com>
2022-01-28 15:26:53 +0800

    change package and remove dist

--- dist/src/middleware/bearer.js
+++
@@ -1,7 +0,0 @@
-// import axios from 'axios';
-// import { decode, Jwt, verify } from 'jsonwebtoken';
-// export default function bearer(req) {
-//     console.log('SSSSSSSSSSSSSSSSSSSSSSSSSS');
-//     console.log(req.headers.authorization);
-// }
-//# sourceMappingURL=bearer.js.map
--- dist/src/utils/errorHandling.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorConstant_1 = require("./errorConstant");
-const getErrorCode = (errorName) => {
-    return errorConstant_1.errType[errorName];
-};
-module.exports = getErrorCode;
-//# sourceMappingURL=errorHandling.js.map
